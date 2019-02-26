254b84ba4c387271f89095295b4cb24cfbd7d8b9
Stas Salmov <s.salmov@gmail.com>
2019-02-26 23:03:53 +0600

    No work ListView

--- app/src/main/java/com/inspiringteam/mrnews/Application.java
+++ app/src/main/java/com/inspiringteam/mrnews/Application.java
@@ -17,6 +17,8 @@ public class Application extends DaggerApplication {
     @Override
     protected AndroidInjector<? extends DaggerApplication> applicationInjector() {
         return DaggerAppComponent.builder().application(this).build();
+
+
     }
 }
 
